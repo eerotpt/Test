c1755fa7c5ca4a38da24ffab41f1632a3d77b258
eero <eero.sakson@tptlive.ee>
2016-11-24 21:50:40 +0200

    21:49

---
+++ src/Test_24_11_2016_linuxmint.java
@@ -0,0 +1,13 @@
+/**
+ * Created by eero on 11/24/16.
+ */
+public class Test_24_11_2016_linuxmint {
+
+    public static int addition(int a,int b){
+        return (a+b);
+    }
+
+    public static void main(String[] args){
+        System.out.print(addition(3,6));
+    }
+}
